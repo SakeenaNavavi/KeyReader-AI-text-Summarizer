ac183f0bbeffe704566b2abd62d56b0048510adc
Sakeena Navavi <140879723+SakeenaNavavi@users.noreply.github.com>
2024-04-08 10:32:38 +0000

    postman code added

--- summarize.js
+++ summarize.js
@@ -3,6 +3,33 @@
 async function summarizeText(text) {
 
   // INSERT CODE SNIPPET FROM POSTMAN BELOW
+  const axios = require('axios');
+  let data = JSON.stringify({
+    "inputs": "Amelia, a curious astronomer, spotted a peculiar comet through her telescope. It emitted a mesmerizing glow and seemed to change course. Determined to investigate, she built a spaceship. On her journey, she encountered cosmic wonders, yet the comet remained elusive. Finally, at the edge of the universe, she understood its secret: the comet was a celestial guide, leading her to discover the boundless wonders within herself. Grateful for the voyage, Amelia returned, sharing her newfound wisdom with the world, inspiring others to explore both the cosmos and the depths of their souls.",
+    "parameters": {
+      "max_length": 100,
+      "min_length": 30
+    }
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': 'Bearer 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'
+    },
+    data : data
+  };
+
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 }
 
